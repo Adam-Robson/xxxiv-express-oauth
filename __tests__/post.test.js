75ba8da24a3e2475ac9012a48754a61e16d85ced
Adam Robson <adamray312@gmail.com>
2022-12-29 19:09:40 -0800

    add salt_rounds to the node yaml file

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const app = require('../lib/app.js');
 const pool = require('../lib/utils/pool.js');
 const Post = require('../lib/models/Post.js');
+
 jest.mock('../lib/services/github');
 
 describe('post', () => {
@@ -11,6 +12,12 @@ describe('post', () => {
   });
   
 
+  it('GET allows authenticated user to veiw all posts', async () => {
+    const agent = request.agent(app);
+    await agent.get('/api/v1/github/callback?code=42');
+    const res = await agent.get('/api/v1/posts');
+    expect(res.status).toEqual(200);
+  })
   it('POST /api/v1/post creates a new post for signed in user', async () => {
     const agent = request.agent(app);
     const user = await agent
@@ -40,9 +47,8 @@ describe('post', () => {
     expect(res.body[0]).toEqual(newPost);
   });
 
-
   afterAll(() => {
     pool.end();
   });
-  
+
 });
